4873cc331bec3e752d332a0ea88fbba8d927e527
sara <sara.souza@bee.com.br>
2020-11-05 16:48:11 -0300

    ajuste

---
+++ config/ifood.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'base_url'      => env('IFOOD_URL', 'https://pos-api.ifood.com.br'), // URL Base da api do Ifood
+    'username'       => env('IFOOD_USERNAME'),
+    'password'    => env('IFOOD_PASSWORD'),
+    'client_id'   => env('CLIENT_ID'),
+    'client_secret' => env('CLIENT_SECRET')
+ ];
--- config/omie.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'base_url'      => env('OMIE_URL', 'https://app.omie.com.br/api/v1'), // URL Base da api do Ifood
-    'app_key'       => env('OMIE_APP_KEY'), // API KEY do Ifood
-    'app_secret'    => env('OMIE_APP_SECRET'),  // API SECRET do Ifood
-];
